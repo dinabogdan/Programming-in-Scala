6172099f728593aadf66e1e66cb07bd612bcb8a7
dinabogdan <bogdan.dina03@gmail.com>
2020-03-12 19:25:43 +0200

    some more play

--- src/main/scala/com/freesoft/programminginscala/HelloWorld.scala
+++ src/main/scala/com/freesoft/programminginscala/HelloWorld.scala
@@ -1,6 +1,10 @@
 package com.freesoft.programminginscala
 
+
 object HelloWorld {
+
+  def factorial(x: BigInt): BigInt =
+    if (x == 0) 1 else x * factorial(x - 1)
 
   def main(args: Array[String]): Unit = {
     var capital = Map(
@@ -9,5 +13,7 @@ object HelloWorld {
     )
     capital += ("Japan" -> "Tokyo")
     println(capital("France"))
+
+    println("10! = " + factorial(10))
   }
 }
