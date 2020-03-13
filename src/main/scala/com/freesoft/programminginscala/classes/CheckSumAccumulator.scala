247a16b07a8fb74281392987252c33d79dd0d14a
dinabogdan <bogdan.dina03@gmail.com>
2020-03-13 22:30:02 +0200

    checksum accumulator

---
+++ src/main/scala/com/freesoft/programminginscala/classes/CheckSumAccumulator.scala
@@ -0,0 +1,44 @@
+package com.freesoft.programminginscala.classes
+
+import scala.collection.mutable
+
+class CheckSumAccumulator {
+  private var sum = 0
+
+  def add(b: Byte): Unit = {
+    sum += b
+  }
+
+  def checksum(): Int = ~(sum & 0xFF) + 1
+
+}
+
+object CheckSumAccumulator {
+
+  private val cache = mutable.Map.empty[String, Int]
+
+  def calculate(s: String): Int =
+    if (cache.contains(s)) cache(s)
+    else {
+      val acc = new CheckSumAccumulator()
+      for (c <- s) acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+    }
+}
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val acc = new CheckSumAccumulator()
+    val csa = new CheckSumAccumulator()
+
+    acc.add(1.toByte)
+    acc.add(2.toByte)
+
+    println(acc.checksum())
+
+    println(CheckSumAccumulator.calculate("The quick brown fox, jumps ... bla... bla ... blaaah"))
+  }
+}
