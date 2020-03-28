7c73f4f4bb607d4dc51bed6a765cee8e0b726cfe
dinabogdan <bogdan.dina03@gmail.com>
2020-03-28 15:57:33 +0200

    functional constructions

--- src/main/scala/com/freesoft/programminginscala/functional/CustomControlStructures.scala
+++ src/main/scala/com/freesoft/programminginscala/functional/CustomControlStructures.scala
@@ -4,9 +4,19 @@ import java.io.{File, PrintWriter}
 
 object CustomControlStructures {
 
+  var assertionEnabled = true
+
   def twice(op: Double => Double, x: Double): Double = op(op(x))
 
-  def withPrintWriter(file: File, op: PrintWriter => Unit): Unit = {
+  def myAssert(predicate: () => Boolean): Unit =
+    if (assertionEnabled && !predicate())
+      throw new AssertionError()
+
+  def byNameAssert(predicate: => Boolean): Unit =
+    if (assertionEnabled && !predicate)
+      throw new AssertionError()
+
+  def withPrintWriter(file: File)(op: PrintWriter => Unit): Unit = {
     val writer = new PrintWriter(file)
     try {
       op(writer)
@@ -15,14 +25,17 @@ object CustomControlStructures {
     }
   }
 
+
   def main(args: Array[String]): Unit = {
 
     println(twice(_ + 1, 5))
 
-    withPrintWriter(
-      new File("date.txt"),
+    val file = new File("date.txt")
+    withPrintWriter(file) {
       writer => writer.println(new java.util.Date)
-    )
+    }
+
+    byNameAssert(3 > 5)
 
   }
 
