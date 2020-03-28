9e1c74d84cae6769649e061e21f6166171777997
dinabogdan <bogdan.dina03@gmail.com>
2020-03-28 14:07:15 +0200

    files matcher

---
+++ src/main/scala/com/freesoft/programminginscala/functional/FileMatcher.scala
@@ -0,0 +1,21 @@
+package com.freesoft.programminginscala.functional
+
+import java.io.File
+
+object FileMatcher {
+
+  private def filesHere = new File(".").listFiles
+
+  def filesEnding(query: String): Array[File] = filesMatching(_ endsWith query)
+
+  def filesContaining(query: String): Array[File] = filesMatching(_ contains query)
+
+  def filesRegex(query: String): Array[File] = filesMatching(_ matches query)
+
+  def filesMatching(matcher: String => Boolean): Array[File] =
+    for {
+      file <- filesHere
+      if matcher(file.getName)
+    } yield file
+
+}
