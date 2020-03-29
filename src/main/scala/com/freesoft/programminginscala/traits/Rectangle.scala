5cca83e8d6f431ad95b29091861309cc11ea9ef5
dinabogdan <bogdan.dina03@gmail.com>
2020-03-29 22:32:56 +0300

    refactor

--- src/main/scala/com/freesoft/programminginscala/traits/Rectangle.scala
+++ src/main/scala/com/freesoft/programminginscala/traits/Rectangle.scala
@@ -1,6 +1,6 @@
 package com.freesoft.programminginscala.traits
 
-class Rectangle(val topLeft: Point, val bottomRight: Point)
-  extends Rectangular {
-
-}
+class Rectangle(
+  val topLeft: Point, val bottomRight: Point
+  ) extends Rectangular
+ 
