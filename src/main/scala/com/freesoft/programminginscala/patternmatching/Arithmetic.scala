55d3c1aee803257283b6f1144832a395bf734ac2
dinabogdan <bogdan.dina03@gmail.com>
2020-03-31 22:54:46 +0300

    simplify all

--- src/main/scala/com/freesoft/programminginscala/patternmatching/Arithmetic.scala
+++ src/main/scala/com/freesoft/programminginscala/patternmatching/Arithmetic.scala
@@ -11,7 +11,7 @@ case class UnOp(operator: String, arg: Expr) extends Expr
 case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
 
 object PatternMatchingMain {
-  
+
   def simplifyTop(expr: Expr): Expr = expr match {
     case UnOp("-", UnOp("-", e)) => e
     case BinOp("+", e, Number(0)) => e
@@ -19,23 +19,38 @@ object PatternMatchingMain {
     case _ => expr
   }
 
+  // pattern guards
+  def simplifyAdd(expr: Expr): Expr = expr match {
+    case BinOp("+", x, y) if x == y => BinOp("*", x, Number(2))
+    case _ => expr
+  }
+
+  def simplifyAll(expr: Expr): Expr = expr match {
+    case UnOp("-", UnOp("-", e)) => simplifyAll(e)
+    case BinOp("+", e, Number(0)) => simplifyAll(e)
+    case BinOp("*", e, Number(1)) => simplifyAll(e)
+    case UnOp(op, e) => UnOp(op, simplifyAll(e))
+    case BinOp(op, l, r) => BinOp(op, simplifyAll(l), simplifyAll(r))
+    case _ => expr
+  }
+
 
   def main(args: Array[String]): Unit = {
 
-    val v = Var("x")
+    //    val v = Var("x")
+    //
+    //    val op = BinOp("+", Number(1), v)
+    //
+    //    println(v.name)
+    //
+    //    println(op)
+    //
+    //    println(simplifyTop(UnOp("-", UnOp("-", Var("x")))))
+    //
+    //    println(simplifyTop(UnOp("+", Var("x"))))
 
-    val op = BinOp("+", Number(1), v)
-
-    println(v.name)
-
-    println(op)
-
-    println(simplifyTop(UnOp("-", UnOp("-", Var("x")))))
-
-    println(simplifyTop(UnOp("+", Var("x"))))
-
-    piMatch(10)
-
+    //    println(UnOp("-", Number(10)))
+    println(simplifyAll(UnOp("-", UnOp("-", Number(10)))))
   }
 
 }
