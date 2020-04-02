67e33a13ed08bf7a44f5ecab280fe260c8333db2
dinabogdan <bogdan.dina03@gmail.com>
2020-04-02 22:48:35 +0300

    divide and conquer

--- src/main/scala/com/freesoft/programminginscala/list/ListExamples.scala
+++ src/main/scala/com/freesoft/programminginscala/list/ListExamples.scala
@@ -6,9 +6,18 @@ object ListExamples {
     if (xs.isEmpty) Nil
     else insert(xs.head, isSort(xs.tail))
 
+  // '::' is called `cons`
+
   private def insert(head: Int, tail: List[Int]): List[Int] =
     if (tail.isEmpty || head <= tail.head) head :: tail
     else tail.head :: insert(head, tail.tail)
+
+  private def append[T](xs: List[T], ys: List[T]): List[T] = {
+    xs match {
+      case List() => ys
+      case x :: xs1 => x :: append(xs1, ys)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
 
@@ -26,6 +35,8 @@ object ListExamples {
     println(t)
     println(u)
     println(rest)
+
+    println(append(List(1, 2, 3), List(5, 6, 7, 8)))
 
   }
 
