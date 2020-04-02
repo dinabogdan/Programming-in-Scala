11a19d953348cdf21b01d036bd9db686ba8c8bc5
dinabogdan <bogdan.dina03@gmail.com>
2020-04-02 23:17:42 +0300

    merge sort

--- src/main/scala/com/freesoft/programminginscala/list/ListExamples.scala
+++ src/main/scala/com/freesoft/programminginscala/list/ListExamples.scala
@@ -19,6 +19,27 @@ object ListExamples {
     }
   }
 
+  private def mergeSort[T](comparison: (T, T) => Boolean)(xs: List[T]): List[T] = {
+
+    def merge(xs: List[T], ys: List[T]): List[T] = {
+      (xs, ys) match {
+        case (Nil, _) => ys
+        case (_, Nil) => xs
+        case (x :: xs1, y :: ys1) =>
+          if (comparison(x, y)) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+    }
+
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(mergeSort(comparison)(ys), mergeSort(comparison)(zs))
+    }
+  }
+
+
   def main(args: Array[String]): Unit = {
 
     println(isSort(List(5, 1, 3, 6, 2, 4, 7)))
@@ -37,6 +58,10 @@ object ListExamples {
     println(rest)
 
     println(append(List(1, 2, 3), List(5, 6, 7, 8)))
+
+    val sortedList = mergeSort((x: Int, y: Int) => x < y)(List(10, 4, 3, 6, 9, 5))
+
+    println(sortedList)
 
   }
 
