3e45f08fd010cbffd460d609fcdfd52b9b980013
dinabogdan <bogdan.dina03@gmail.com>
2020-04-04 23:41:27 +0300

    information hiding

--- src/main/scala/com/freesoft/programminginscala/functional/Queue.scala
+++ src/main/scala/com/freesoft/programminginscala/functional/Queue.scala
@@ -1,21 +1,36 @@
 package com.freesoft.programminginscala.functional
 
-class Queue[T](
-                private val leading: List[T],
-                private val trailing: List[T]
-              ) {
+trait Queue[T] {
+  def head: T
 
-  private def mirror() = if (leading.isEmpty)
-    new Queue(trailing.reverse, Nil)
-  else this
+  def tail: Queue[T]
 
-  def head: T = mirror().leading.head
+  def enqueue(element: T): Queue[T]
+}
 
-  def tail: Queue[T] = {
-    val q = mirror()
-    new Queue(q.leading.tail, q.trailing)
+object Queue {
+
+  def apply[T](xs: T*): Queue[T] = new QueueImpl[T](xs.toList, Nil)
+
+  private class QueueImpl[T](
+                              private val leading: List[T],
+                              private val trailing: List[T]
+                            ) extends Queue[T] {
+
+    private def mirror() = if (leading.isEmpty)
+      new QueueImpl(trailing.reverse, Nil)
+    else this
+
+    def head: T = mirror().leading.head
+
+    def tail: Queue[T] = {
+      val q = mirror()
+      new QueueImpl(q.leading.tail, q.trailing)
+    }
+
+    def enqueue(element: T) = new QueueImpl(leading, element :: trailing)
   }
 
-  def enqueue(element: T) = new Queue(leading, element :: trailing)
-
 }
+
+
