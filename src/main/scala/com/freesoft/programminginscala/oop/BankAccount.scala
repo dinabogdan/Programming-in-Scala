5b516a1cab3136913bc079472be33d551993c7b1
dinabogdan <bogdan.dina03@gmail.com>
2020-04-04 21:23:08 +0300

    mutable objects

---
+++ src/main/scala/com/freesoft/programminginscala/oop/BankAccount.scala
@@ -0,0 +1,21 @@
+package com.freesoft.programminginscala.oop
+
+class BankAccount {
+
+  private var bal: Int = 0
+
+  def balance: Int = bal
+
+  def deposit(amount: Int): Unit = {
+    require(amount > 0)
+    bal += amount
+  }
+
+  def withdraw(amount: Int): Boolean =
+    if (amount > bal) false
+    else {
+      bal -= amount
+      true
+    }
+
+}
